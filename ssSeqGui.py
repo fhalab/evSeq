a3e743e9131db638ad4a5475306e7e0d032de8c3
Bruce <bwittman@caltech.edu>
2020-03-24 15:43:16 -0700

    Forced cwd change

--- ssSeqGui.py
+++ ssSeqGui.py
@@ -2,6 +2,10 @@
 
 # Import subprocess
 import subprocess
+import os
+
+# Change cwd
+os.chdir(os.path.dirname(__file__))
 
 # Run the sequence
 subprocess.run('conda run -n ssSeq python ssSeqSupport/GUI.py', shell=True)
