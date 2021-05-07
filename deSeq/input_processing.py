1c66252764347d97f2e14786514ff3e65522eca6
Kadina Johnston <kjohnston@caltech.edu>
2021-05-07 11:44:22 -0700

    major restructuring of package architecture and setup.py implementation

---
+++ deSeq/input_processing.py
@@ -0,0 +1,307 @@
+
+# Import deSeq objects
+from .globals import ALLOWED_WELLS, HOMEDIR
+from .logging import log_error, log_input_file
+from .input_validation import check_ref_seqs, check_index_map
+
+# Import other required modules
+import re
+import os
+import gzip
+import shutil
+import pandas as pd
+import numpy as np
+from glob import glob
+
+# Write a function that builds the output directory structure
+def build_output_dirs(cl_args):
+    
+    # Build the folder structure if it does not exist
+    if not os.path.exists(cl_args["output"]):
+        os.makedirs(cl_args["output"])
+    
+    # Build required folders
+    os.mkdir(os.path.join(cl_args["output"], "Qualities"))
+    
+    # Build folders that only occur if we don't stop too early
+    if not cl_args["analysis_only"]:
+        fastq_folder = os.path.join(cl_args["output"], "ParsedFilteredFastqs")
+        os.makedirs(os.path.join(fastq_folder, "F"))
+        os.makedirs(os.path.join(fastq_folder, "R"))
+    if not cl_args["stop_after_fastq"]:
+        os.mkdir(os.path.join(cl_args["output"], "OutputCounts"))
+        os.mkdir(os.path.join(cl_args["output"], "Platemaps"))
+     
+    # Build optional folders for the outputs
+    if cl_args["return_alignments"]:
+        os.mkdir(os.path.join(cl_args["output"], "Alignments"))
+
+# Write a function that matches forward and reverse reads in a passed in folder
+def find_matches(input_folder):
+    
+    # Glob all potential sequencing files in the directory
+    seqfiles = glob(os.path.join(input_folder, "*fastq*"))
+    
+    # Define a generic regex to identify forward and reverse reads in the folder
+    f_regex = re.compile("(.*)_R1_.*")
+    r_regex = re.compile("(.*)_R2_.*")
+    
+    # Extract all forward filenames from the seqfiles
+    f_searches = [f_regex.search(filename) for filename in seqfiles]
+    f_filenames = [filename for filename, test in zip(seqfiles, f_searches) if test]
+    f_filenames_set = set(f_filenames)
+    
+    # Identify remaining files
+    remaining_filenames = [filename for filename in seqfiles 
+                           if filename not in f_filenames_set]
+    
+    # Create a dictionary storing all forward files. Make the values None for now
+    filepairs = {filename: None for filename in f_filenames}
+    
+    # Create a dictionary linking a root name to f_filename
+    root_to_f = {f_regex.search(filename).group(1): filename for 
+                 filename in f_filenames}
+    
+    # Loop over the remaining filenames and match to the forward files. Also record
+    # those filenames with no match
+    unmatched_files = []
+    for filename in remaining_filenames:
+        
+        # Search to see if this is a reverse file
+        reverse_search = r_regex.search(filename)
+        
+        # Get the root name check to see if there is a matching forward file
+        if reverse_search:
+                    
+            # Get the rootname
+            rootname = reverse_search.group(1)
+            
+            # Confirm that the rootname is present in root_to_f
+            if rootname in root_to_f:
+            
+                # Match the reverse filename to the forward filename
+                filepairs[root_to_f[rootname]] = filename
+                
+            # If we can't find a match, record this as an unmatched file
+            else:
+                unmatched_files.append(filename) 
+            
+        # If we can't find a match, record this as an unmatched file
+        else:
+            unmatched_files.append(filename) 
+            
+    # Check to see which filepairs still have 'None' as their value. Record 
+    # these as unmatched files
+    final_filepairs = {}
+    for filekey, fileval in filepairs.items():
+        
+        # If fileval is None, record the filekey as an unmatched file
+        if fileval is None:
+            unmatched_files.append(filekey)
+            
+        # Otherwise add to the final filepair dictionary
+        else:
+            final_filepairs[filekey] = fileval
+    
+    # If final_filepairs is 0, throw and error and terminate the program
+    n_pairs = len(final_filepairs)
+    if n_pairs == 0:
+        log_error("Could not match forward and reverse files. "
+                  "Make sure they have appropriate names.")
+    
+    # If more than 1 pair is identified, throw and error and terminate the program
+    if n_pairs > 1:
+        log_error("More than 1 pair of sequencing files found in input directory.")
+    
+    # Return the dictionary matching files as well as the unmatched files
+    forward_file, reverse_file = list(final_filepairs.items())[0]
+    return forward_file, reverse_file, unmatched_files
+
+# This function unzips gz files
+def unzip_gz(filename):
+
+    # Find the name without the ".gz"
+    new_name = os.path.splitext(filename)[0]
+    
+    # Unzip and resave the file
+    with gzip.open(filename, 'rb') as f_in:
+        with open(new_name, 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+                
+    # Return the new name
+    return new_name
+
+# Write a function that loads and checks the contents of IndexMap.csv
+def load_dual_inds():
+    
+    # Identify the expected location of IndexMap.csv
+    index_map_loc = os.path.join(HOMEDIR, "IndexMap.csv")
+    
+    # Check to make sure that IndexMap.csv can be found as a file
+    if not os.path.exists(index_map_loc):
+        print(index_map_loc)
+        # Log an error
+        log_error("Cannot find 'IndexMap.csv' in ssSeq/ssSeqSupport/")
+        
+    # Load the file
+    index_df = pd.read_csv(index_map_loc)
+    
+    # Check the index file integrity
+    check_index_map(index_df)
+    
+    # Return the index_df
+    return index_df
+
+# Write a function that loads the reference sequence file, checks the integrity
+# of the input information, and generates an appropriate reference sequence dataframe
+def load_ref_seq(cl_args):
+    
+    # Load the reference sequence using pandas
+    refseq_df = pd.read_csv(cl_args["refseq"])
+    
+    # Check the validity of the reference sequence file
+    check_ref_seqs(refseq_df, cl_args["detailed_refseq"])
+    
+    # Convert all sequences to uppercase
+    anycase_refseqs = refseq_df.ReferenceSequence.values.tolist()
+    uppercase_refseqs = [refseq.upper() for refseq in anycase_refseqs]
+    refseq_df["ReferenceSequence"] = uppercase_refseqs
+    
+    # If this is not a detailed reference sequence, expand it
+    if not cl_args["detailed_refseq"]:
+    
+        # Expand to a detailed reference sequence dataframe
+        updated_ref_array = []
+        for row in refseq_df.itertuples(index = False):
+            updated_ref_array.extend([[row.PlateName, row.IndexPlate, well,
+                                       row.ReferenceSequence, row.InFrameBase,
+                                       row.BpIndStart, row.AaIndStart]
+                                      for well in ALLOWED_WELLS])
+
+        # Define the complete reference sequence dataframe
+        refseq_df = pd.DataFrame(updated_ref_array,
+                                 columns = ("PlateName", "IndexPlate", "Well",
+                                            "ReferenceSequence", "InFrameBase",
+                                            "BpIndStart", "AaIndStart"))
+    
+    # Output the ref_seq_df
+    return refseq_df
+
+# Write a function that finds variable positions in a reference sequence.
+def find_codons_variable_positions(refseq, inframe_bp, plate, well):
+    
+    # Get the number of codons and number of variable positions
+    n_codons = refseq.count("NNN")
+    n_positions = refseq.count("N")
+    
+    # Check to be sure the number of positions is divisible by 3 and divides
+    # to the number of codons
+    if (n_positions % 3) != 0:
+        log_error(f"Error for {plate}-{well} refseq:"
+                  "Must enter `N` in groups of 3 to signify codon.")
+    assert (n_positions / 3) == n_codons, "Mismatch in number of codons and number of variable positions."
+
+    # If no codons are found, return an empty array
+    if n_codons == 0:
+        return np.array([], dtype = int)
+    
+    # Split the reference sequence
+    split_seqs = refseq.split("NNN")
+
+    # Loop over the splits and make checks
+    variable_positions = []
+    variable_counter = 0
+    for frag_ind, fragment in enumerate(split_seqs[:-1]):
+
+        # Make sure fragments other than the first are divisible by 3, if not, then
+        # the sections are not in frame
+        frag_len = len(fragment)
+        if (frag_ind > 0) and (not (frag_len % 3 == 0)):
+            log_error(f"Error for {plate}-{well} refseq:"
+                      "Specified variable positions are not in the same reading frame.")
+
+        # Extend the variable positions. It will be extended by 
+        # the fragment length
+        variable_counter += frag_len
+        for _ in range(3):
+            variable_positions.append(variable_counter)
+            variable_counter += 1
+
+    # Convert to an array. Make sure everything is sorted.
+    variable_positions = np.array(variable_positions)
+    variable_positions.sort()
+
+    # Make sure that all variable positions are "N" and that we found the appropriate
+    # number of codons
+    assert len(variable_positions) == n_positions, "Missing variable positions"
+    assert all(refseq[pos] == "N" for pos in variable_positions), "Error in variable position calculation"
+        
+    # Validate that the provided in frame bp is correct
+    if (variable_positions[0] % 3) != (inframe_bp - 1):
+        log_error(f"Error for {plate}-{well} refseq:"
+                  "The provided in frame base does not match the frame of the provided variable codons.")
+    
+    return variable_positions
+
+# Write a function which constructs global objects from the index_df and 
+# ref_seq_df
+def construct_bcs_to_refseq(refseq_df, index_df):
+    
+    # Join on plate and well
+    merged_dfs = refseq_df.merge(index_df, on = ("IndexPlate", "Well"))
+
+    # Make sure the length of the refseq df is the same as the length of the 
+    # merged dfs. We do not want to have lost anything in the merge.
+    if len(merged_dfs) != len(refseq_df):
+        log_error("Merging refseq_df and index_df failed. Values lost in merge.")
+
+    # For each reference sequence, link barcode to plate information. Also 
+    # calculate any expected mutagenesis positions
+    bc_to_ref_plate_well = {}
+    for row in merged_dfs.itertuples(index = False):
+
+        # Find variable positions
+        variable_positions = find_codons_variable_positions(row.ReferenceSequence,
+                                                            row.InFrameBase,
+                                                            row.IndexPlate,
+                                                            row.Well)
+
+        # Map barcode to reference sequence, plate, and well
+        bc_to_ref_plate_well[(row.FBC, row.RBC)] = {"IndexPlate": row.IndexPlate,
+                                                    "PlateName": row.PlateName,
+                                                    "Well": row.Well,
+                                                    "ReferenceSequence": row.ReferenceSequence,
+                                                    "InFrameBase": row.InFrameBase,
+                                                    "ExpectedVariablePositions": variable_positions,
+                                                    "BpIndStart": row.BpIndStart,
+                                                    "AAIndStart": row.AaIndStart}
+        
+    # Return the mapping of barcode pair to well information
+    return bc_to_ref_plate_well
+
+# Write a function that loads everything. This wraps all functions defined above.
+def load_all(cl_args):
+    
+    # Find matching forward and reverse reads in a folder if the files are not
+    # explicitly passed in together
+    if cl_args["fastq_r"] == "":
+        forward_file, reverse_file, unmatched_files = find_matches(cl_args["folder"])
+    
+    # Otherwise, assign files
+    else:
+        forward_file = cl_args["folder"]
+        reverse_file = cl_args["fastq_r"]
+        unmatched_files = []
+    
+    # Log the identified input files
+    log_input_file(forward_file, reverse_file, unmatched_files)
+    
+    # Load the index dataframe and reference sequence files
+    index_df = load_dual_inds()
+    refseq_df = load_ref_seq(cl_args)
+    
+    # Merge the two dataframes and generate the dataframe info needed to be 
+    # passed in to all wells
+    bc_to_ref_plate_well = construct_bcs_to_refseq(refseq_df, index_df)
+    
+    return forward_file, reverse_file, bc_to_ref_plate_well
---
+++ deSeq/run_deSeq.py
@@ -0,0 +1,257 @@
+# Import deSeq objects
+from .logging import log_info, log_warning
+from .input_validation import check_args
+from .input_processing import load_all, unzip_gz
+from .seq_pair import SeqPair
+from .well import Well
+from .data_visualization import (generate_read_qual_chart, 
+                                generate_sequencing_heatmaps,
+                                save_heatmap_to_file)
+
+# Import other required modules
+import os
+import numpy as np
+import pandas as pd
+import scipy.stats as ss
+from Bio import SeqIO
+from itertools import chain
+from functools import partial
+from multiprocessing import Pool
+from tqdm import tqdm
+            
+# Write a function for loading and pairing fastq files
+def build_seqpairs(f_loc, r_loc):
+
+    # Unzip the files if need be
+    if "fastq.gz" in f_loc:
+        f_loc = unzip_gz(f_loc)
+    if "fastq.gz" in r_loc:
+        r_loc = unzip_gz(r_loc)
+
+    # Create a dictionary that links id to sequence object
+    id_to_reads = {}
+    
+    # Load and parse forward reads
+    print("Loading forward reads...")
+    all_f_recs = list(SeqIO.parse(f_loc, "fastq"))
+    for f_record in tqdm(all_f_recs, desc = "Parsing forward reads..."):
+        temp_record = SeqPair()
+        temp_record.assign_f(f_record)
+        id_to_reads[f_record.id] = temp_record
+    
+    # Associate reverse reads with the forward
+    print("Loading reverse reads...")
+    all_r_recs = list(SeqIO.parse(r_loc, "fastq"))
+    for r_record in tqdm(all_r_recs, "Pairing reverse reads..."):
+
+        # If there is no partern in id_to_reads, create a new object 
+        # and continue
+        if r_record.id not in id_to_reads:
+            temp_record = SeqPair()
+            temp_record.assign_r(r_record)
+            id_to_reads[r_record.id] = temp_record
+
+        # Otherwise, attach the reverse record
+        else:
+            id_to_reads[r_record.id].assign_r(r_record)
+            
+    # Return all records
+    return tuple(id_to_reads.values())
+
+# Write a function for filtering out bad seqpairs
+def qc_seqpairs(all_seqpairs, read_length, length_cutoff, average_q_cutoff):
+    
+    print("Running read qc...")
+    
+    # If we don't have the read length determine it
+    if read_length is None:
+
+        # Get the most common read length. We will assign this as our read length
+        all_readlengths = np.array([seqpair.read_lengths() for seqpair in all_seqpairs])
+        read_length = ss.mode(all_readlengths, axis = None, nan_policy = "omit").mode[0]
+        
+        log_info(f"A read length of {read_length} was calculated for this run")
+        
+    # Calculate the read filter
+    read_filter = read_length * length_cutoff
+        
+    # Run QC on every read
+    for seqpair in all_seqpairs:
+        seqpair.qc_reads(read_filter, average_q_cutoff)
+    
+    # Eliminate any duds, which are those seqpairs with both a forward and a reverse that failed qc
+    no_duds = tuple(filter(lambda x: not x.is_dud(), all_seqpairs))
+    
+    return no_duds
+
+# Write a function for assigning seqpairs to a well
+def assign_seqpairs_to_well(filtered_seqpairs, bc_to_ref_plate_well, savedir):
+
+    # Loop over all seqpairs and assign to wells
+    print("Assigning sequences to wells...")
+    well_pairs = {}
+    for pair in filtered_seqpairs:
+
+        # Grab the well ID and see if it is a real well. Continue
+        # if it is not. "Fake" wells are those that result from 
+        # sequencing errors
+        well_id = (pair.f_barcode, pair.r_barcode)
+        if well_id not in bc_to_ref_plate_well:
+            continue
+        
+        # Check to see if we have seen this well already.
+        # If we have seen it, append to growing list. If we have not,
+        # start a new list
+        if well_id in well_pairs:
+            well_pairs[well_id].append(pair)
+        else:
+            well_pairs[well_id] = [pair]
+            
+    # Now build and return the well objects
+    return [Well(pair, bc_to_ref_plate_well[well_id], savedir) 
+            for well_id, pair in well_pairs.items()] 
+
+# Write a function that can process a single well
+def process_well(well, return_alignments = False, bp_q_cutoff = 30, 
+                 variable_thresh = 0.1, variable_count = 1):
+    
+    # Define a frequency warning variable
+    freq_warning = ""
+    
+    # Align
+    well.align()
+
+    # Analyze alignments. 
+    has_reads = well.analyze_alignments(bp_q_cutoff, variable_count)
+
+    # If we don't have any reads that passed
+    # QC, skip straight to analyzing counts. 
+    if has_reads:
+        # Build count matrices
+        well.build_unit_count_matrices()
+
+        # Identify variable positions
+        freq_warning = well.identify_variable_positions(variable_thresh)
+
+    # Analyze reads with decoupled counts
+    well.analyze_unpaired_counts(variable_thresh)
+
+    # Analyze reads with coupled counts
+    well.analyze_paired_counts(variable_thresh, variable_count)
+
+    # If we are returning alignments, generate them
+    if return_alignments:
+        formatted_alignments = well.format_alignments()
+    else:
+        formatted_alignments = None
+
+    # Return relevant information for downstream processing
+    return (well.unpaired_bp_output, well.unpaired_bp_output_max,
+            well.unpaired_aa_output, well.unpaired_aa_output_max,
+            well.paired_bp_output, well.paired_aa_output, 
+            formatted_alignments, freq_warning) 
+    
+def format_and_save_outputs(well_results, saveloc, return_alignments):
+
+    # Write a function that processes the output of analyzing all wells
+    unpacked_output = tuple(zip(*well_results))
+
+    # Concatenate all dataframes
+    full_dfs = tuple(pd.concat(df_list, ignore_index = True) for df_list in unpacked_output[:-2])
+
+    # Get just the max of each of the paired outputs
+    max_outs = [None, None]
+    for i, paired_df in enumerate(full_dfs[4:]):
+
+        # Get the columns of interest
+        limited_df = paired_df.loc[:, ["IndexPlate", "Well", "AlignmentFrequency"]]
+
+        # Group by plate and well
+        grouped_df = limited_df.groupby(by = ["IndexPlate", "Well"])
+        max_outs[i] = paired_df.loc[grouped_df.idxmax().AlignmentFrequency.values].copy()
+
+    # Loop over all dataframes, sort by plate and well, and save
+    savenames = ("Bases_Decoupled_All.csv", "Bases_Decoupled_Max.csv",
+                "AminoAcids_Decoupled_All.csv", "AminoAcids_Decoupled_Max.csv",
+                 "Bases_Coupled_All.csv", "Combos_Coupled_All.csv",
+                 "Bases_Coupled_Max.csv", "Combos_Coupled_Max.csv")
+    for savename, output_df in zip(savenames, chain(full_dfs, max_outs)):
+
+        # Sort by plate and well
+        output_df.sort_values(by = ["IndexPlate", "Well"], inplace = True)
+
+        # Save the dataframe
+        output_df.to_csv(os.path.join(saveloc, "OutputCounts", savename), index = False)
+        
+    # Generate heatmaps from the Combos_Coupled_Max dataframe
+    heatmaps = generate_sequencing_heatmaps(max_outs[-1])
+    save_heatmap_to_file(heatmaps, saveloc)
+
+    # Loop over and save all alignments if asked to do so
+    if return_alignments:
+        for savename, savestr in unpacked_output[-2]:
+            with open(savename, "w") as f:
+                f.write(savestr)   
+                
+    # Report all warnings
+    for warning in unpacked_output[-1]:
+        if warning != "":
+            log_warning(f"High mutational frequency in {warning}. You may "
+                        "want to check alignments for accuracy.") 
+
+# Write a function that runs deSeq
+def run_deseq(cl_args):
+   
+    # Check the input arguments
+    check_args(cl_args)
+    
+    # Identify sequencing files and load reference sequence information
+    forward_file, reverse_file, bc_to_ref_plate_well = load_all(cl_args)
+    
+    # Pair all sequences
+    all_seqpairs = build_seqpairs(forward_file, reverse_file)
+    
+    # Generate quality plots
+    generate_read_qual_chart(all_seqpairs, cl_args["output"])
+    
+    # Return if we stop after plot qualities
+    if cl_args["analysis_only"]:
+        return
+    
+    # Run QC on the seqpairs
+    filtered_seqpairs = qc_seqpairs(all_seqpairs, cl_args["read_length"],
+                                    cl_args["length_cutoff"], 
+                                    cl_args["average_q_cutoff"])
+    
+    # Assign seqpairs to a well
+    all_wells = assign_seqpairs_to_well(filtered_seqpairs, 
+                                        bc_to_ref_plate_well,
+                                        cl_args["output"])
+    
+    # Save the fastq files
+    print("Saving fastq files...")
+    for well in all_wells:
+        well.write_fastqs()
+
+    # Return if we stop after fastq
+    if cl_args["stop_after_fastq"]:
+        return
+        
+    # Complete the multiprocessing function, then process all wells
+    complete_multiprocessor = partial(process_well, 
+                                      bp_q_cutoff = cl_args["bp_q_cutoff"],
+                                      return_alignments = cl_args["return_alignments"],
+                                      variable_thresh = cl_args["variable_thresh"],
+                                     variable_count = cl_args["variable_count"])
+        
+    # Multiprocess to handle wells
+    with Pool(cl_args["jobs"]) as p:
+        processed_well_results = list(tqdm(p.imap_unordered(complete_multiprocessor, all_wells),
+                                      desc = "Processing wells:", total = len(all_wells)))  
+    
+    # Handle processed output. This saves the summary dataframes, generates 
+    # platemaps, and saves alignments (if requested)
+    print("Saving outputs to disk...")
+    format_and_save_outputs(processed_well_results, 
+                            cl_args["output"],
+                            cl_args["return_alignments"])
