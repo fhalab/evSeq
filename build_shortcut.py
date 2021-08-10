51d963475a85af32c83fcf6f4d6cd810cac9f162
palmhjell <palmhjel@caltech.edu>
2021-08-10 12:09:54 -0700

    Remove `%~dp0%` from envrunner-env.bat file if/when created
    Former-commit-id: 8f363876c80fa5a2980a6400ab246c747b8acf39

--- build_shortcut.py
+++ build_shortcut.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
-
+"""
+Create a clickable shortcut to run evSeq from the corect environment
+"""
 from pyshortcuts import make_shortcut
+import os
+import sys
+import glob
 
 def main():
     make_shortcut(
@@ -10,6 +15,27 @@ def main():
         startmenu=False,
         icon='icons/program_icon'
     )
+
+    # Check for Windows and remove `%~dp0%` from .bat file
+    file_match = r'envrunner-*.bat'
+    file_path = os.path.join(sys.prefix, 'Scripts')
+    files = glob.glob(os.path.join(file_path, file_match))
+    if files:
+        if len(files) == 1:
+            file = files[0]
+            new_file_list = []
+            with open(file, 'r') as f:
+                for line in f:
+                    if '%~dp0%' in line:
+                        line = line.replace('%~dp0%', '')
+                    new_file_list.append(line)
+            with open(file, 'w') as f:
+                for line in new_file_list:
+                    f.write(line)
+        else:
+            # Error?
+            pass
+
 
 if __name__ == '__main__':
     main()
